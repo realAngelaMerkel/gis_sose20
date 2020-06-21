67bbd323c03ed60b76064f57b1b97aac36b82b2e
Luca Senne <luca.senne@hs-furtwangen.de>
2020-06-21 17:23:31 +0200

    Aufgabe 8 angefangen

---
+++ Aufgabe08/script.ts
@@ -0,0 +1,19 @@
+namespace A08Server {
+    let submit: HTMLButtonElement = <HTMLButtonElement>document.getElementById("abgabe");
+    submit.addEventListener("click", server);
+
+    async function server(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://angiegissose2020.herokuapp.com";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+
+        await fetch(url);
+
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+    }
+}
