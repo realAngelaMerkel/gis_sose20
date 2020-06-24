1f4c4dad08faf407aeab1317ea7f2904b9a905db
Luca Senne <luca.senne@hs-furtwangen.de>
2020-06-24 11:38:25 +0200

    js fix

--- Aufgabe07/script.js
+++ Aufgabe07/script.js
@@ -11,7 +11,7 @@ var Aufgabe07;
         if (produktCounter >= 0) {
             document.getElementById("cart_count")?.appendChild(cartDiv);
         }
-        produktCounter += 1;
+        produktCounter += 0;
         cartDiv.innerHTML = produktCounter + "";
         artikelSumme += parseInt(_event.currentTarget?.getAttribute("preis"));
         localStorage.setItem("summe", artikelSumme.toString());
@@ -48,76 +48,75 @@ var Aufgabe07;
         showDiv.style.display = "flex";
     }
     function createArtikel() {
-        var countKoks = 0;
-        var countNutten = 0;
+        var countProduct = 0;
         for (let i of Aufgabe07.produkte) {
-            countKoks++;
             if (i.category == "1") {
                 var div = document.createElement("div");
-                div.setAttribute("id", "koks" + countKoks);
-                div.setAttribute("index", countKoks);
+                div.setAttribute("id", "koks" + countProduct);
+                div.setAttribute("index", countProduct);
                 document.getElementById("koks").appendChild(div);
                 var img = document.createElement("img");
                 img.setAttribute("src", i.img);
                 img.setAttribute("alt", i.alt);
-                document.getElementById("koks" + countKoks).appendChild(img);
+                document.getElementById("koks" + countProduct).appendChild(img);
                 var h2 = document.createElement("h2");
                 var h2text = document.createTextNode(i.name);
                 h2.appendChild(h2text);
-                document.getElementById("koks" + countKoks).appendChild(h2);
+                document.getElementById("koks" + countProduct).appendChild(h2);
                 var p = document.createElement("p");
                 var ptext = document.createTextNode(i.description);
                 p.appendChild(ptext);
-                document.getElementById("koks" + countKoks).appendChild(p);
+                document.getElementById("koks" + countProduct).appendChild(p);
                 var p2 = document.createElement("p");
                 p2.setAttribute("class", "Preis");
                 var p2text = document.createTextNode(i.preis + "€");
                 p2.appendChild(p2text);
-                document.getElementById("koks" + countKoks).appendChild(p2);
+                document.getElementById("koks" + countProduct).appendChild(p2);
                 var input = document.createElement("input");
                 input.setAttribute("type", "submit");
                 input.setAttribute("value", "In den Warenkorb");
                 input.addEventListener("click", addtoCart);
-                input.setAttribute("elementID", "koks" + countKoks);
+                input.setAttribute("elementID", "koks" + countProduct);
                 input.setAttribute("price", i.preis);
-                input.setAttribute("index", countKoks);
-                document.getElementById("koks" + countKoks).appendChild(input);
+                input.setAttribute("index", countProduct);
+                document.getElementById("koks" + countProduct).appendChild(input);
             }
-            countNutten++;
             if (i.category == "2") {
                 var div = document.createElement("div");
-                div.setAttribute("id", "nutten" + countNutten);
+                div.setAttribute("id", "nutten" + countProduct);
+                div.setAttribute("index", countProduct);
                 document.getElementById("nutten").appendChild(div);
                 var img = document.createElement("img");
                 img.setAttribute("src", i.img);
                 img.setAttribute("alt", i.alt);
-                document.getElementById("nutten" + countNutten).appendChild(img);
+                document.getElementById("nutten" + countProduct).appendChild(img);
                 var h2 = document.createElement("h2");
                 var h2text = document.createTextNode(i.name);
                 h2.appendChild(h2text);
-                document.getElementById("nutten" + countNutten).appendChild(h2);
+                document.getElementById("nutten" + countProduct).appendChild(h2);
                 var p = document.createElement("p");
                 var ptext = document.createTextNode(i.description);
                 p.appendChild(ptext);
-                document.getElementById("nutten" + countNutten).appendChild(p);
+                document.getElementById("nutten" + countProduct).appendChild(p);
                 var p2 = document.createElement("p");
                 p2.setAttribute("class", "Preis");
                 var p2text = document.createTextNode(i.preis + "€");
                 p2.appendChild(p2text);
-                document.getElementById("nutten" + countNutten).appendChild(p2);
+                document.getElementById("nutten" + countProduct).appendChild(p2);
                 var input = document.createElement("input");
                 input.setAttribute("type", "submit");
                 input.setAttribute("value", "In die Kiste");
-                input.setAttribute("elementID", "nutten" + countNutten);
+                input.setAttribute("elementID", "nutten" + countProduct);
                 input.setAttribute("price", i.preis);
                 input.addEventListener("click", addtoCart);
-                input.setAttribute("index", countNutten);
-                document.getElementById("nutten" + countNutten).appendChild(input);
+                input.setAttribute("index", countProduct);
+                document.getElementById("nutten" + countProduct).appendChild(input);
             }
             var koksButton = document.getElementById("sortKoks");
             koksButton.addEventListener("click", sortKoks);
             var nuttenButton = document.getElementById("sortNutten");
             nuttenButton.addEventListener("click", sortNutten);
+            countProduct++;
         }
     }
     Aufgabe07.createArtikel = createArtikel;
